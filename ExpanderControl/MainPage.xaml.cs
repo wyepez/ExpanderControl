f541cfae951926bc6a74ce045a8d3ddf6c6bac67
Washington Xavier Yépez Yépez <wyepez@outlook.com>
2017-11-29 16:40:38 +0100

    Add project files.

---
+++ ExpanderControl/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace ExpanderControl
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new MyContentDialog();
+            await dialog.ShowAsync();
+        }
+    }
+}
---
+++ ExpanderControl/MyContentDialog.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace ExpanderControl
+{
+    public sealed partial class MyContentDialog : ContentDialog
+    {
+        public MyContentDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void ContentDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+        }
+    }
+}
